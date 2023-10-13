384527075f9261581691ef20bd784ed8dda5cfe3
Adam Taylor <afranklint16@gmail.com>
2023-10-13 11:08:15 -0600

    Extract code into a function

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 // Highlight the tab at the top corresponding to the page we've selected
-const path = window.location.pathname;
-const page_name = path.split('/').pop().replace('.html', '');
-const element = document.getElementById(page_name);
-if (element) {
-    element.style.backgroundColor = "#AAAAAA"
+function highlight_current_page_tab() {
+    const path = window.location.pathname;
+    const page_name = path.split('/').pop().replace('.html', '');
+    const element = document.getElementById(page_name);
+    if (element) {
+        element.style.backgroundColor = "#AAAAAA"
+    }
 }
+highlight_current_page_tab();
