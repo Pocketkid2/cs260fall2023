0e13ad11f541e4c5c926feecfa8b8e0f2ac7b44b
Adam Taylor <afranklint16@gmail.com>
2023-10-24 14:39:24 -0600

    Add some code to every page to remove the unapplicable tabs from the nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
+const current_user_key = "current_user";
+
 // Highlight the tab at the top corresponding to the page we've selected
 function highlight_current_page_tab() {
+    
     const path = window.location.pathname;
     const page_name = path.split('/').pop().replace('.html', '');
     const element = document.getElementById(page_name);
@@ -7,4 +10,38 @@ function highlight_current_page_tab() {
         element.style.backgroundColor = "#AAAAAA"
     }
 }
+
 highlight_current_page_tab();
+
+// Remove the navigation tabs relating to the pages that are available or unavailable
+function remove_unusable_navigation_tabs() {
+    const current_username = localStorage.getItem(current_user_key);
+
+    if (current_username === null) {
+
+        // Remove profile tab
+        const profile_button = document.getElementById("header-nav-bar-profile").parentElement.remove();
+
+    } else {
+
+        const nav_list = document.getElementById("header-nav-bar-index").parentElement;
+
+        // Remove login and signup
+        document.getElementById("header-nav-bar-login").parentElement.remove();
+        document.getElementById("header-nav-bar-signup").parentElement.remove();
+
+        // Add sign out tab
+        var signout_li = document.createElement("li");
+        var signout_a = document.createElement("a");
+        signout_a.setAttribute("id", "header-nav-bar-signout");
+        signout_a.addEventListener("click", function(event) {
+            event.preventDefault();
+            localStorage.removeItem(current_user_key);
+            location.reload(true);
+        });
+        signout_li.appendChild(signout_a);
+        nav_list.appendChild(signout_li);
+    }
+}
+
+remove_unusable_navigation_tabs();
