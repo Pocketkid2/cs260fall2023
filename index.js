322b2240a6e8bc005f4fda07a679ea9fdeb46c00
Adam Taylor <afranklint16@gmail.com>
2023-11-08 15:08:25 -0700

    Add massive logging statement, primarily for learning and debug purposes

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`\n\nRequest received (time = ${new Date(Date.now()).toLocaleString()})`);
+    console.log(`\tMethod: ${req.method}`);
+    console.log(`\tURL: ${req.originalUrl}`);
+    console.log(`\tBody: ${JSON.stringify(req.body)}`);
+    console.log(`\tParams: ${JSON.stringify(req.params)}`);
+    console.log(`\tQuery: ${JSON.stringify(req.query)}`);
+    console.log(`\tHeaders: ${JSON.stringify(req.headers)}`);
+    console.log(`\tCookies: ${JSON.stringify(req.cookies)}`);
+    console.log(`\tIP: ${req.ip}`);
+    console.log(`\tProtocol: ${req.protocol}`);
+    next();
+});
+
 app.use(express.static('public'));
 
 var api_router = express.Router();
