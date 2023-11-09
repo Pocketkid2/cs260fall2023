e1605c367115f91afaabe53d69e2e3eafb630cba
Adam Taylor <afranklint16@gmail.com>
2023-11-09 14:39:03 -0700

    Plug authentication endpoints into data module (they work simply now)

--- auth_router.js
+++ auth_router.js
@@ -1,7 +1,6 @@
 var express = require('express');
+var data = require('./data');
 var auth_router = express.Router();
-
-users = [];
 
 auth_router.use((req, res, next) => {
     console.log(`\nAuth Request (time = ${new Date(Date.now()).toLocaleString()})`)
@@ -9,63 +8,79 @@ auth_router.use((req, res, next) => {
 });
 
 auth_router.post('/signup', (req, res) => {
-    if (!req.body.username || 
-        !req.body.password ||
-        !req.body.first_name ||
-        !req.body.last_name ||
-        !req.body.gender ||
-        !req.body.birth_date) {
-        console.log("\tRejecting request due to missing fields");
-        res.status(400).send("Missing required fields");
+    
+    // Make sure the important fields are in the signup request
+    if (!req.body.username) {
+        console.log("\tRejecting request due to missing username");
+        res.status(400).send("Missing username");
+        return;
+    }
+    if (!req.body.password) {
+        console.log("\tRejecting request due to missing password");
+        res.status(400).send("Missing password");
         return;
     }
 
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username == req.body.username) {
-            console.log("\tRejecting request due to duplicate username");
-            res.status(400).send("Duplicate username");
-            return;
-        }
+    // Make sure the username is unique
+    if (data.user_exists(req.body.username)) {
+        console.log("\tRejecting request due to existing username");
+        res.status(409).send("Username already exists");
+        return;
     }
 
     const user_object = {
         username: req.body.username,
         password: req.body.password,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        gender: req.body.gender,
-        birth_date: req.body.birth_date
     };
-    users.push(user_object);
 
-    console.log("\tUser added");
-    res.status(200).send("User added");
+    data.add_user(user_object);
+
+    console.log("\tUser signed up");
+    res.status(200).end();
 });
 
-auth_router.get('/login', (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        console.log("\tRejecting request due to missing fields");
-        res.status(400).send("Missing username or password");
-        return;
-    }
+auth_router.post('/login', (req, res) => {
 
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username == req.body.username &&
-            users[i].password == req.body.password) {
-            console.log("Found user");
-            res.status(200).send("User found");
+    // If we get an empty login request but we have a valid auth token cookie, return success
+    if (req.cookies.auth_token != undefined) {
+        const username = data.authenticate_token(req.cookies.auth_token);
+        if (username) {
+            console.log("\tUser already logged in");
+            res.status(200).end();
             return;
         }
     }
 
-    console.log('Did not find user');
-    res.status(404).send("User not found");
+    // If we don't receive a cookie, login the usual way
+    if (!req.body.username) {
+        console.log("\tRejecting request due to missing username");
+        res.status(400).send("Missing username");
+        return;
+    }
+    if (!req.body.password) {
+        console.log("\tRejecting request due to missing password");
+        res.status(400).send("Missing password");
+        return;
+    }
+
+    // Attempt authentication and return the result
+    const auth_token = data.authenticate_credentials(req.body.username, req.body.password);
+    if (auth_token) {
+        console.log("\tUser logged in");
+        res.cookie('auth_token', auth_token, { sameSite: 'strict' });
+        res.status(200).end();
+    } else {
+        console.log("\tRejecting request due to invalid username/password");
+        res.status(401).send("Invalid username/password");
+    }
 });
 
 auth_router.get('/list', (req, res) => {
-    console.log("\tListing users");
-    console.log('\t' + users);
-    res.send(users);
+    console.log("\nListing Users:");
+    data.list_users();
+    console.log("\nListing Tokens:");
+    data.list_tokens();
+    res.end();
 });
 
 module.exports = auth_router;
