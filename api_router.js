f97b5d42caf8b6b9b89688d17b7560aa2020d096
Adam Taylor <afranklint16@gmail.com>
2023-11-10 10:56:05 -0700

    Implement api calls

--- api_router.js
+++ api_router.js
@@ -8,23 +8,142 @@ api_router.use((req, res, next) => {
 });
 
 api_router.get('/list/:list(watchlist|favorites)', (req, res) => {
-    console.log(`\tRetrieve entire list for ${req.params.list}`);
-    res.send(`Retrieve entire list for ${req.params.list}`);
+
+    const auth_token = req.cookies.auth_token;
+
+    if (auth_token === undefined) {
+        console.log("\tRejecting request due to missing auth token");
+        res.status(401).send("Missing auth token");
+        return;
+    }
+
+    const username = data.authenticate_token(auth_token);
+
+    if (username === null) {
+        console.log("\tRejecting request due to invalid auth token");
+        res.status(401).send("Invalid auth token");
+        return;
+    }
+
+    const list = data.get_list(username, req.params.list);
+
+    console.log(`\tSending ${req.params.list} for ${username} as ${JSON.stringify(list)}`);
+    res.status(200).send(list);
 });
 
 api_router.get('/exists/:list(watchlist|favorites)', (req, res) => {
-    console.log(`\tCheck if film exists in ${req.params.list}`);
-    res.send(`Check if film exists in ${req.params.list}`);
+
+    const auth_token = req.cookies.auth_token;
+
+    if (auth_token === undefined) {
+        console.log("\tRejecting request due to missing auth token");
+        res.status(401).send("Missing auth token");
+        return;
+    }
+
+    const username = data.authenticate_token(auth_token);
+
+    if (username === null) {
+        console.log("\tRejecting request due to invalid auth token");
+        res.status(401).send("Invalid auth token");
+        return;
+    }
+
+    const film = req.body.film;
+
+    if (film === undefined) {
+        console.log("\tRejecting request due to missing film");
+        res.status(400).send("Missing film");
+        return;
+    }
+
+    const list = data.get_list(username, req.params.list);
+
+    if (list.includes(film)) {
+        console.log(`\tFilm ${film} exists in ${req.params.list} for ${username}`);
+        res.status(200).end();
+    } else {
+        console.log(`\tFilm ${film} does not exist in ${req.params.list} for ${username}`);
+        res.status(404).end();
+    }
 });
 
 api_router.post('/add/:list(watchlist|favorites)', (req, res) => {
-    console.log(`\tAdd film to ${req.params.list}`);
-    res.send(`Add film to ${req.params.list}`);
+
+    const auth_token = req.cookies.auth_token;
+
+    if (auth_token === undefined) {
+        console.log("\tRejecting request due to missing auth token");
+        res.status(401).send("Missing auth token");
+        return;
+    }
+
+    const username = data.authenticate_token(auth_token);
+
+    if (username === null) {
+        console.log("\tRejecting request due to invalid auth token");
+        res.status(401).send("Invalid auth token");
+        return;
+    }
+
+    const film = req.body.film;
+
+    if (film === undefined) {
+        console.log("\tRejecting request due to missing film");
+        res.status(400).send("Missing film");
+        return;
+    }
+
+    const list = data.get_list(username, req.params.list);
+
+    if (list.includes(film)) {
+        console.log(`\tFilm ${film} already exists in ${req.params.list} for ${username}`);
+        res.status(409).send("Film already exists");
+        return;
+    }
+
+    list.push(film);
+    console.log(`\tAdded film ${film} to ${req.params.list} for ${username}`);
+    res.status(200).end();
 });
 
 api_router.delete('/remove/:list(watchlist|favorites)', (req, res) => {
-    console.log(`\tRemove film from ${req.params.list}`);
-    res.send(`Remove film from ${req.params.list}`);
+
+    const auth_token = req.cookies.auth_token;
+
+    if (auth_token === undefined) {
+        console.log("\tRejecting request due to missing auth token");
+        res.status(401).send("Missing auth token");
+        return;
+    }
+
+    const username = data.authenticate_token(auth_token);
+
+    if (username === null) {
+        console.log("\tRejecting request due to invalid auth token");
+        res.status(401).send("Invalid auth token");
+        return;
+    }
+
+    const film = req.body.film;
+
+    if (film === undefined) {
+        console.log("\tRejecting request due to missing film");
+        res.status(400).send("Missing film");
+        return;
+    }
+
+    const list = data.get_list(username, req.params.list);
+
+    if (!list.includes(film)) {
+        console.log(`\tFilm ${film} does not exist in ${req.params.list} for ${username}`);
+        res.status(404).send("Film does not exist");
+        return;
+    }
+
+    list.splice(list.indexOf(film), 1);
+    console.log(`\tRemoved film ${film} from ${req.params.list} for ${username}`);
+    res.status(200).end();
 });
 
 module.exports = api_router;
