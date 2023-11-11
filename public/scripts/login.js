08fac8c77f85aa124f653ac893d491c24e424382
Adam Taylor <afranklint16@gmail.com>
2023-11-11 09:56:27 -0700

    Submit or reset form based on API request instead of local storage

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,30 +1,27 @@
 function login(event) {
-    var available_users = localStorage.getItem("available_users");
-    if (available_users === null) {
-        event.preventDefault();
-        alert("There are no users on this site!");
-        return;
-    }
-    available_users = JSON.parse(available_users);
-
-    const username_input = document.getElementById("username").value;
-    const password_input = document.getElementById("password").value;
-    
-    for (i = 0; i < available_users.length; i++) {
-        const user = available_users[i];
-
-        if (user.username === username_input) {
-            if (user.password === password_input) {
-                localStorage.setItem("current_user", user.username);
-                return;
-            } else {
-                event.preventDefault();
-                alert("Incorrect password");
-                return;
-            }
-        }
-    }
-
     event.preventDefault();
-    alert("Incorrect username");
+
+    const login_credentials = {
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value
+    };
+
+    fetch('/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(login_credentials)
+    })
+    .then(response => {
+        if (response.status === 200) {
+            document.getElementById("login-form").submit();
+        } else {
+            alert('Invalid username/password combination. Please try again.');
+            document.getElementById("login-form").reset();
+        }
+    })
+    .catch(error => {
+        console.log('There was a problem with the fetch operation: ' + error.message);
+    });
 }
