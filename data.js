586b0a2261186a0f95d74aa56b515599dde60c49
Adam Taylor <afranklint16@gmail.com>
2023-11-13 15:48:15 -0700

    Rewrite everything to be asynchronous and pull from database, and added some new functions to help

--- data.js
+++ data.js
@@ -17,9 +17,6 @@ const auth_collection = db.collection('auth');
     process.exit(1);
 });
 
-var users = [];
-var auth_tokens = {};
-
 module.exports = {
     authenticate_credentials: authenticate_credentials,
     authenticate_token: authenticate_token,
@@ -27,68 +24,104 @@ module.exports = {
     add_user: add_user,
     get_info: get_info,
     get_list: get_list,
+    film_exists: film_exists,
+    add_film: add_film,
+    remove_film: remove_film,
     delete_token: delete_token,
     list_users: list_users,     // DEBUG
     list_tokens: list_tokens,   // DEBUG
 };
 
 function list_users() {
-    console.log(JSON.stringify(users));
+    user_collection.find().toArray().then(users => {
+        console.log(JSON.stringify(users));
+    });
 }
 
 function list_tokens() {
-    console.log(JSON.stringify(auth_tokens));
+    auth_collection.find().toArray().then(tokens => {
+        console.log(JSON.stringify(tokens));
+    });
 }
 
-function get_info(username) {
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username === username) {
-            const user_copy = Object.assign({}, users[i]);
-            delete user_copy.username;
-            delete user_copy.password;
-            return user_copy;
-        }
+async function get_info(username) {
+    const user = await user_collection.findOne({ username: username });
+    if (user) {
+        const { password, ...user_copy } = user;
+        return user_copy;
     }
     return null;
 }
 
-function get_list(username, list_name) {
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username === username) {
-            if (users[i][list_name] === undefined) {
-                users[i][list_name] = [];
-            }
-            return users[i][list_name];
-        }
+async function get_list(username, list_name) {
+    const user = await user_collection.findOne({ username: username });
+    if (user && user[list_name]) {
+        return user[list_name];
     }
     return null;
 }
 
-function user_exists(username) {
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username === username) {
-            return true;
-        }
+async function film_exists(username, list_name, film_name) {
+    const user = await user_collection.findOne({ username: username });
+    if (user && user[list_name]) {
+        return user[list_name].includes(film_name);
     }
     return false;
 }
 
-function add_user(user_object) {
-    users.push(user_object);
+async function add_film(username, list_name, film_name) {
+    let user = await user_collection.findOne({ username: username });
+    if (user) {
+        if (user[list_name] === undefined) {
+            user[list_name] = [];
+        }
+        user[list_name].push(film_name);
+        await user_collection.updateOne({ username: username }, { $set: user });
+        console.log(`Added film ${film_name} to list ${list_name} for user ${username}`);
+    }
+}
+
+async function remove_film(username, list_name, film_name) {
+    let user = await user_collection.findOne({ username: username });
+    if (user) {
+        if (user[list_name] === undefined) {
+            user[list_name] = [];
+        }
+        user[list_name] = user[list_name].filter(film => film !== film_name);
+        await user_collection.updateOne({ username: username }, { $set: user });
+        console.log(`Removed film ${film_name} from list ${list_name} for user ${username}`);
+    }
+}
+
+async function user_exists(username) {
+    const user = await user_collection.findOne({ username: username });
+    if (user) {
+        console.log("Found user " + JSON.stringify(user));
+        return true;
+    }
+    return false;
+}
+
+async function add_user(user_object) {
+    // users.push(user_object);
+    await user_collection.insertOne(user_object);
+    console.log(`Added user ${user_object.username}`);
 }
 
 // Attempts to authenticate with the given credentials.
 //  If successful, returns an auth token.
 //  If unsuccessful, returns null.
-function authenticate_credentials(username, password) {
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username == username &&
-            users[i].password == password) {
-            if (auth_tokens[username]) {
-                return auth_tokens[username];
-            } else {
-                return create_auth_token(username);
-            }
+async function authenticate_credentials(username, password) {
+    const user = await user_collection.findOne({ username: username, password: password });
+    if (user) {
+        const auth = await auth_collection.findOne({ username: username });
+        if (auth) {
+            console.log(`Returning existing authtoken ${auth.token}`);
+            return auth.token;
+        } else {
+            const token = create_token(username);
+            console.log(`Created new authtoken ${token}`);
+            return token;
         }
     }
     return null;
@@ -97,22 +130,23 @@ function authenticate_credentials(username, password) {
 // Attempts to authenticate with the given auth token.
 //  If successful, returns the username.
 //  If unsuccessful, returns null.
-function authenticate_token(token) {
-    for (var username in auth_tokens) {
-        if (auth_tokens[username] === token) {
-            return username;
-        }
+async function authenticate_token(token) {
+    const auth = await auth_collection.findOne({ token: token });
+    if (auth) {
+        return auth.username;
     }
     return null;
 }
 
 function delete_token(username) {
-    delete auth_tokens[username];
+    auth_collection.deleteOne({ username: username });
 }
 
-function create_auth_token(username) {
+function create_token(username) {
     var token = generate_random_auth_token();
-    auth_tokens[username] = token;
+    
+    auth_collection.insertOne({ username: username, token: token });
+
     return token;
 }
 
