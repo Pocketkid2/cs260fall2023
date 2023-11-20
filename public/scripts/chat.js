3a20cb35256baa904000056ea94695f8868af4bd
Adam Taylor <afranklint16@gmail.com>
2023-11-20 15:02:32 -0700

    Use websocket to load messages on frontend

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,44 +1,128 @@
-function populate_user_list() {
-    var user_list_element = document.getElementById("active-user-list");
-    var user_name_element = document.createElement("li");
-    user_name_element.innerText = localStorage.getItem("current_user");
-    user_list_element.appendChild(user_name_element);
-}
+const maximum_message_length = 1000;
 
-populate_user_list();
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const ws_url = `${protocol}://${window.location.host}/ws`;
 
+console.log('Connecting to ' + ws_url);
+
+let socket = new WebSocket(ws_url);
+
+socket.onopen = (event) => {
+    console.log('Socket opened');
+};
+
+socket.onmessage = async (event) => {
+    console.log('Socket message received: ' + event.data);
+    const text = event.data;
+    const message_object = JSON.parse(text);
+    switch (message_object.type) {
+        case "new_user":
+            add_user(message_object.username);
+            display_custom_message(`${message_object.username} joined the chat`);
+            break;
+        case "message":
+            display_chat_message(message_object.username, message_object.message);
+            break;
+        case "user_left":
+            remove_user(message_object.username);
+            display_custom_message(`${message_object.username} left the chat`);
+            break;
+        default:
+            console.log(`Unknown message type ${message_object.type}`);
+            break;
+    }
+};
+
+socket.onclose = (event) => {
+    console.log('Socket closed');
+};
+
+// What to do when the send button is pressed, or the enter button is pressed
 function send_chat_message(event) {
+
+    // Don't actually submit
     event.preventDefault();
+
+    // Read the message
     const chat_message = document.getElementById("message").value;
-    if (chat_message.length <= 0) {
+
+    // Check that the message is valid size
+    if (chat_message.length <= 0 || chat_message.length > maximum_message_length) {
         return;
     }
-    var message_box_element = document.getElementById("chat-messages");
-    var message_header_list = message_box_element.querySelectorAll("dt");
-    
-    const username = localStorage.getItem("current_user");
 
-    if (message_header_list.length > 0) {
-        const chat_header_element = message_header_list[message_header_list.length - 1];
-        if (username === chat_header_element.innerHTML) {
-            append_chat_message(chat_message);
-            return;
-        }
-    }
-    
-    // Append a new header
-    var message_header = document.createElement("dt");
-    message_header.innerHTML = username;
-    message_box_element.appendChild(message_header);
+    // Clear input box
+    document.getElementById("message").value = '';
 
-    append_chat_message(chat_message);
+    // Send message over socket
+    socket.send(JSON.stringify({ type: 'message', message: chat_message }));
 }
 
-function append_chat_message(message) {
-    var message_element = document.createElement("dd");
-    message_element.innerHTML = message;
-    var message_box_element = document.getElementById("chat-messages");
-    message_box_element.appendChild(message_element);
+function display_custom_message(message) {
 
-    message_box_element.scrollTop = message_box_element.scrollHeight;
+    // Grab the chatbox
+    var chat_box_element = document.getElementById("chat-messages");
+
+    // Create a new message element and display it
+    const custom_message = document.createElement("p");
+    custom_message.innerText = message;
+    chat_box_element.appendChild(custom_message);
+
+    // And scroll down so it's visible
+    chat_box_element.scrollTop = chat_box_element.scrollHeight;
+}
+
+// Adds the message to the chat display
+function display_chat_message(username, message) {
+
+    // Grab the chatbox
+    var chat_box_element = document.getElementById("chat-messages");
+
+    // Grab all elements that separate messages by username
+    var list_of_username_chat_headers = chat_box_element.querySelectorAll("dt");
+
+    // If we have any
+    if (list_of_username_chat_headers.length > 0) {
+
+        // Grab the latest separator
+        const most_recent_username_chat_header = list_of_username_chat_headers[list_of_username_chat_headers.length - 1];
+        
+        // If it is not the one whose message we want to display, create a new separator
+        if (most_recent_username_chat_header.innerHTML !== username) {
+            var username_chat_header = document.createElement("dt");
+            username_chat_header.innerText = username;
+            chat_box_element.appendChild(username_chat_header);
+        }
+    } else {
+        var username_chat_header = document.createElement("dt");
+        username_chat_header.innerText = username;
+        chat_box_element.appendChild(username_chat_header);
+    }
+
+    // Now append the chat message
+    var chat_message_element = document.createElement("dd");
+    chat_message_element.innerHTML = message;
+    chat_box_element.appendChild(chat_message_element);
+
+    // And scroll down so it's visible
+    chat_box_element.scrollTop = chat_box_element.scrollHeight;
+}
+
+// Adds the username to the active user display
+function add_user(username) {
+    var user_element = document.createElement("li");
+    user_element.innerText = username;
+    var user_list_element = document.getElementById("active-user-list");
+    user_list_element.appendChild(user_element);
+}
+
+// Removes the username from the active user display
+function remove_user(username) {
+    var user_list_element = document.getElementById("active-user-list");
+    var user_element_list = user_list_element.querySelectorAll("li");
+    user_element_list.forEach(user_element => {
+        if (user_element.innerText === username) {
+            user_element.remove();
+        }
+    });
 }
